e1f1d9229cc5c8d40d9ae57de49eecc7e4aad7a3
EthanHarkins <eharkins@ltu.edu>
2023-02-14 17:36:38 -0500

    Parralax/Movement

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+
+    float moveSpeed = 3;
+
+    bool moveUp;
+    bool moveDown;
+    bool moveLeft;
+    bool moveRight;
+
+    void Start()
+    {
+
+
+    }
+
+
+    void Update()
+    {
+        moveUp = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        moveDown = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+        moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+    }
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        float moveAmount = moveSpeed * Time.fixedDeltaTime;
+        Vector2 move = Vector2.zero;
+
+        if (moveUp)
+        {
+            move.y += moveAmount;
+        }
+
+        if (moveDown)
+        {
+            move.y -= moveAmount;
+        }
+
+        if (moveLeft)
+        {
+            move.x -= moveAmount;
+        }
+
+        if (moveRight)
+        {
+            move.x += moveAmount;
+        }
+
+        pos += move;
+
+        transform.position = pos;
+    }
+}
+
