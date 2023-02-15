f124ae70b06f58d8b30046d82bcf80329ef225fc
EthanHarkins <eharkins@ltu.edu>
2023-02-14 23:00:07 -0500

    Bullet+EnemyStart

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
 
+    Gun[] guns;
+
+    
+
     float moveSpeed = 3;
 
     bool moveUp;
     bool moveDown;
     bool moveLeft;
     bool moveRight;
+    bool shoot;
+    bool speedUp;
 
     void Start()
     {
-
+        guns = transform.GetComponentsInChildren<Gun>();
 
     }
 
@@ -25,12 +31,31 @@ public class Ship : MonoBehaviour
         moveDown = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
         moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
         moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+
+        speedUp = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+        shoot = Input.GetKeyDown(KeyCode.LeftControl);
+        if (shoot)
+        {
+            shoot = false;
+            foreach(Gun gun in guns)
+            {
+                gun.Shoot();
+            }
+        }
     }
     private void FixedUpdate()
     {
+
         Vector2 pos = transform.position;
 
         float moveAmount = moveSpeed * Time.fixedDeltaTime;
+
+        if (speedUp)
+        {
+            moveAmount *= 3;
+        }
+
         Vector2 move = Vector2.zero;
 
         if (moveUp)
